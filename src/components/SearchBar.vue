976a730beba2df889624c1bc5e7f7204d2f249ca
Brady Bridges <bradyjbridges@gmail.com>
2020-01-11 00:59:46 -0700

    Implement image search functionality

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input @change="handleChange" v-bind:placeholder="defaultSearch"/>
+    <button @click="$emit('search', search)">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  props: ['defaultSearch'],
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    handleChange(e) {
+      this.search = e.target.value;
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
